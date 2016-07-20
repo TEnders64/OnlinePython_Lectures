2840d9052049856aecb82a13bcb37c93560093d1
Todd Enders <tenders64@gmail.com>
2016-07-20 15:55:52 -0700

    lecture 3 complete

---
+++ week3/toddsProject/server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, session, flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+
+app = Flask(__name__)
+app.secret_key = "ThisIsSecret!" #for session
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/users', methods=["POST"])
+def create():
+    errors = False
+    if len(request.form['username']) < 2:
+        flash("Username must be at least 2 characters")
+        errors = True
+    if len(request.form['email']) == 0:
+        flash("Email is required")
+        errors = True
+    elif not EMAIL_REGEX.match(request.form['email']):
+        flash("Invalid Email Address!")
+        errors = True
+    if errors:
+        return redirect('/')
+    else:
+        session['username'] = request.form['username']
+        session['email'] = request.form['email']
+        return redirect('/success')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+app.run(debug=True)
