b838a6809c3f350a69d7489eebbd401d2f569e97
Todd Enders <tenders64@gmail.com>
2016-07-27 15:32:20 -0700

    lecture 3 with bcrypt complete

--- week4/flaskMySqlMagic/server.py
+++ week4/flaskMySqlMagic/server.py
@@ -7,8 +7,34 @@ import bcrypt
 mysql = MySQLConnector(app, 'world')
 
 @app.route('/')
+def reroute():
+    return redirect('/countries')
+
+@app.route('/countries')
 def index():
-    result = mysql.query_db("SELECT * FROM countries ORDER BY population DESC LIMIT 10")
+    result = mysql.query_db("SELECT * FROM countries ORDER BY population DESC")
     return render_template('index.html', result=result)
+
+@app.route('/countries/new')
+def new():
+    return render_template('new.html')
+
+@app.route('/countries', methods=["POST"])
+def create():
+    query = "INSERT INTO countries (name, population, region) VALUES (:name, :population, :region)"
+    data = {
+        'name': request.form['name'],
+        'population': request.form['population'],
+        'region': request.form['region']
+    }
+    country_id = mysql.query_db(query, data)
+    # print '/countries/'+str(country_id)
+    return redirect('/countries/'+str(country_id))
+
+@app.route('/countries/<country_id>')
+def show(country_id):
+    result = mysql.query_db("SELECT * FROM countries WHERE id = {}".format(country_id))
+    # [{'name':'Legoland', 'population':500000}]
+    return render_template('show.html', country=result[0])
 
 app.run(debug=True)
