f49ed3044088e0536800248bf393a72c764e27ab
Todd Enders <tenders64@gmail.com>
2016-08-10 14:10:12 -0700

    Django lecture 3

--- week6/newDemoProject/apps/testApp/views.py
+++ week6/newDemoProject/apps/testApp/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'testApp/index.html')
+    try:
+        request.session['count'] += 1
+    except:
+        request.session['count'] = 0
+
+    context = {
+        'count' : request.session['count']
+    }
+    return render(request, 'testApp/index.html', context)
+
+def create(request):
+    request.session['username'] = request.POST['name']
+    print request.method
+    print request.POST['name']
+    return redirect('/')
