ca3920abfc548156320fc3a250bf5ed3b4d1c07d
Todd Enders <tenders64@gmail.com>
2016-09-08 12:47:49 -0700

    starting AJAX

--- AJAX_OOP/FlaskAPI/server.py
+++ AJAX_OOP/FlaskAPI/server.py
@@ -2,10 +2,16 @@ from flask import Flask, request, render_template, redirect, jsonify
 from mysqlconnection import MySQLConnector
 app = Flask(__name__)
 
-mysql = MySQLConnector(app, 'world')
+mysql = MySQLConnector(app, 'world') #make sure the db name matches your world database
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/countries')
+def countries():
+    countries = mysql.query("SELECT * FROM countries")
+    print countries
+    return jsonify(countries)
+    
 app.run(debug=True)
