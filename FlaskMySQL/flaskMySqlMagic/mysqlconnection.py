3426a62811662f14aabf54c902ec3d9a21e22266
Todd Enders <tenders64@gmail.com>
2017-01-30 16:59:28 -0800

    updated activity

--- FlaskMySQL/flaskMySqlMagic/mysqlconnection.py
+++ FlaskMySQL/flaskMySqlMagic/mysqlconnection.py
@@ -1,4 +1,4 @@
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 # Create a class that will give us an object that we can use to connect to a database
 class MySQLConnection(object):
